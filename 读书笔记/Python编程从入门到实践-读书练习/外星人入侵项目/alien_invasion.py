0e6c1faa65180b0cb6acc9eb317ddb1420603fc7
o_ofine <o_ofine@github.com>
2024-05-11 16:08:27 +0800

    2024.5.11外星人入侵游戏项目全部完成

--- 读书笔记/Python编程从入门到实践-读书练习/外星人入侵项目/alien_invasion.py
+++ 读书笔记/Python编程从入门到实践-读书练习/外星人入侵项目/alien_invasion.py
@@ -6,6 +6,8 @@
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
@@ -18,8 +20,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 穿件一个用于存储游戏统计信息的实例
+    # 创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 穿件一个用于存储游戏统计信息的实例,并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
@@ -32,14 +38,14 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_evevts(ai_settings, screen, ship, bullets)
+        gf.check_evevts(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
